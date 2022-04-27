71ae315a9e645f5d627a43a109c336a9d40396d3
Tiffany <twarre30@gmail.com>
2022-04-27 10:01:43 -0500

    ideas

--- resistor-color-trio.js
+++ resistor-color-trio.js
@@ -1,1 +1,51 @@
-export function decodedResistorValue() {}
+const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
+
+const colorCode = (color) => COLORS.indexOf(color);
+
+const UNITS = [
+  {
+    name: "giga",
+    numberOfZeros: 9,
+    representation: "000000000",
+  },
+  {
+    name: "mega",
+    numberOfZeros: 6,
+    representation: "000000",
+  },
+  {
+    name: "kilo",
+    numberOfZeros: 3,
+    representation: "000",
+  },
+];
+
+export const decodedResistorValue = ([tens, ones, exponent]) => {
+  const numericValue =
+    (colorCode(tens) * 10 + colorCode(ones)) * 10 ** colorCode(exponent);
+  const stringifiedValue = numericValue.toString();
+
+  let readableUnit = "";
+  let readableValue = stringifiedValue;
+
+  for (const unit of UNITS) {
+    if (stringifiedValue.endsWith(unit.representation)) {
+      readableUnit = unit.name;
+      readableValue = readableValue.slice(0, -unit.numberOfZeros);
+      break;
+    }
+  }
+
+  return `${readableValue} ${readableUnit}ohms`;
+};
